f911e6c1bfd6cb3b40fad4254b33adb804ff933d
astashenkov <ya.astashenkov@gmail.com>
2022-01-02 16:06:18 +0300

    Selection sort

---
+++ selection_sort.js
@@ -0,0 +1,29 @@
+'use strict'
+
+function selectionSort(arr) {
+    let i = 0,
+        newArr = [];
+
+    while (arr.length) {
+        i = findIndexMin(arr);
+        newArr.push(arr[i]);
+        arr.splice(i, 1);
+    }
+
+    return newArr;
+}
+
+function findIndexMin(arr) {
+    let index = 0,
+        min = arr[0];
+    arr.forEach((element, i) => {
+        if (element < min) {
+            min = element;
+            index = i;
+        }
+    });
+    return index;
+}
+
+let arr = [-6, 463, 88, 91, 4000, 24, 9, 677, 30, 5, 0];
+console.log(selectionSort(arr));
